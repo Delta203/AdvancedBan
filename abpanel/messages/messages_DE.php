cc5eeea52a22d29b078e7d816a5356bd772a402a
Delta203 <ddelta203@gmail.com>
2024-04-16 12:03:20 +0200

    Update messages_DE.php

--- abpanel/messages/messages_DE.php
+++ abpanel/messages/messages_DE.php
@@ -1,11 +1,24 @@
 <?php
-$messages_title = "Erweitertes Ban-Panel";
+# Placeholders:
+#   %player%    | Player name
+#   %uuid%      | Player uuid
+#   %from%      | From player name
+#   %fromUUID%  | From player uuid
+#   %key%       | Session key*
+#   %server%    | Server
+#   %date%      | Date
+#   %reason%    | Reason
+#   %bans%      | Amount of bans
+#   %mutes%     | Amount of mutes
+#   %reports%   | Amount of reports
+
+$messages_title = "Advanced Ban Panel";
 
 $messages_name = "Name";
 $messages_uuid = "UUID";
 $messages_key = "Schlüssel";
-$messages_login = "Anmeldung";
-$messages_logout = "Abmeldung";
+$messages_login = "Anmelden";
+$messages_logout = "Abmelden";
 $messages_player = "Spieler";
 $messages_search = "Suche";
 $messages_date = "Datum";
@@ -16,9 +29,9 @@ $messages_duration = "Dauer";
 $messages_server = "Server";
 $messages_message = "Nachricht";
 
-$messages_footer = "Erweitertes Ban - 2024 von Delta203";
+$messages_footer = "Advanced Ban - 2024 von Delta203";
 
-$messages_navbar_home = "Startseite";
+$messages_navbar_home = "Home";
 
 $messages_sidebar_logged = "Angemeldeter Benutzer: <b>%player%</b>";
 $messages_sidebar_uuid = "UUID: <b>%uuid%</b>";
@@ -31,14 +44,14 @@ $messages_invalid_session_info = "Entweder stimmt die UUID nicht mit dem Schlüs
 $messages_not_found = "Nicht gefunden";
 $messages_not_found_info = "Die angeforderte URL wurde auf diesem Server nicht gefunden.";
 
-$messages_alert_success = "Sie haben den Bericht erfolgreich abgeschlossen!";
-$messages_alert_jumped = "Sie werden nun zum Server <b>%server%</b> wechseln!";
-$messages_alert_unbanned = "Sie haben <b>%player%</b> erfolgreich entbannt!<br>Bitte warten Sie einige Sekunden, damit der Unban-Vorgang abgeschlossen werden kann.";
-$messages_alert_unmuted = "Sie haben <b>%player%</b> erfolgreich entmuted!<br>Bitte warten Sie einige Sekunden, damit der Unmute-Vorgang abgeschlossen werden kann.";
+$messages_alert_success = "Du hast den Report erfolgreich bearbeitet!";
+$messages_alert_jumped = "Du wirst nun zum Server <b>%server%</b> springen!";
+$messages_alert_unbanned = "Du hast <b>%player%</b> erfolgreich entbannt!<br>Bitte warte einige Sekunden, damit der Unban Vorgang abgeschlossen werden kann.";
+$messages_alert_unmuted = "Du hast <b>%player%</b> erfolgreich entmuted!<br>Bitte warte einige Sekunden, damit der Unmute Vorgang abgeschlossen werden kann.";
 
-$messages_index_manage_reports = "Berichte verwalten";
-$messages_index_manage_reports_info = "Derzeit sind %reports% offene Berichte vorhanden.";
-$messages_index_manage_reports_button = "Bericht öffnen";
+$messages_index_manage_reports = "Reports verwalten";
+$messages_index_manage_reports_info = "Derzeit sind %reports% offene Reports vorhanden.";
+$messages_index_manage_reports_button = "Report öffnen";
 $messages_index_check_player = "Spieler überprüfen";
 $messages_index_check_player_info = "Suche nach Spieler UUID oder Namen.";
 $messages_index_global_bans = "Globale Bans:";
@@ -52,19 +65,19 @@ $messages_check_unban = "Spieler entbannen";
 $messages_check_unmute = "Spieler entmuten";
 $messages_check_name = "Name: <b>%player%</b>";
 $messages_check_uuid = "UUID: <b>%uuid%</b>";
-$messages_check_namemc = "NameMC-Profil:";
-$messages_check_reports = "Berichte:";
+$messages_check_namemc = "NameMC Profil:";
+$messages_check_reports = "Reports:";
 $messages_check_registered = "Registriert: <b>%date%</b>";
 $messages_check_server = "Server: <b>%server%</b>";
 $messages_check_banned = "Gebannt:";
 $messages_check_muted = "Gemuted:";
-$messages_check_ban_mute = "Ban- & Mute-Verlauf";
+$messages_check_ban_mute = "Ban & Mute Verlauf";
 $messages_check_ban_mute_bans = "Bans: <b>%bans%</b>";
 $messages_check_ban_mute_mutes = "Mutes: <b>%mutes%</b>";
 
-$messages_report = "Bericht";
-$messages_report_jump = "Zum Server wechseln";
-$messages_report_close = "Bericht schließen";
+$messages_report = "Report";
+$messages_report_jump = "Zum Server springen";
+$messages_report_close = "Report schließen";
 $messages_report_reason = "Grund: <b>%reason%</b>";
 $messages_report_name = "Name:";
 $messages_report_uuid = "UUID: <b>%uuid%</b>";
@@ -75,5 +88,5 @@ $messages_report_from_uuid = "Von UUID: <b>%fromUUID%</b>";
 $messages_report_punish = "Bestrafe den Spieler <b>%player%</b>!";
 $messages_report_punish_checked = "Alles überprüft";
 $messages_report_punish_button = "Spieler bestrafen";
-$messages_report_chatlog = "Chatprotokoll";
+$messages_report_chatlog = "Chat Log";
 ?>
