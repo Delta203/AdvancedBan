2f717fd48a4ec8f94f921e013d378756f0a857dc
Delta203 <ddelta203@gmail.com>
2024-04-16 12:07:24 +0200

    Update messages_DE.php

--- abpanel/messages/messages_DE.php
+++ abpanel/messages/messages_DE.php
@@ -14,6 +14,7 @@
 
 $messages_title = "Advanced Ban Panel";
 
+# Universal
 $messages_name = "Name";
 $messages_uuid = "UUID";
 $messages_key = "Schlüssel";
@@ -29,6 +30,7 @@ $messages_duration = "Dauer";
 $messages_server = "Server";
 $messages_message = "Nachricht";
 
+# Assets
 $messages_footer = "Advanced Ban - 2024 von Delta203";
 
 $messages_navbar_home = "Home";
@@ -38,17 +40,20 @@ $messages_sidebar_uuid = "UUID: <b>%uuid%</b>";
 $messages_sidebar_session = "Sitzung: <b>%key%</b>";
 $messages_sidebar_server = "Server: <b>%server%</b>";
 
+# Error
 $messages_invalid_session = "Ungültige Sitzung";
 $messages_invalid_session_info = "Entweder stimmt die UUID nicht mit dem Schlüssel überein, der Schlüssel ist falsch oder die Sitzung ist abgelaufen.";
 
 $messages_not_found = "Nicht gefunden";
 $messages_not_found_info = "Die angeforderte URL wurde auf diesem Server nicht gefunden.";
 
+# Alerts
 $messages_alert_success = "Du hast den Report erfolgreich bearbeitet!";
 $messages_alert_jumped = "Du wirst nun zum Server <b>%server%</b> springen!";
 $messages_alert_unbanned = "Du hast <b>%player%</b> erfolgreich entbannt!<br>Bitte warte einige Sekunden, damit der Unban Vorgang abgeschlossen werden kann.";
 $messages_alert_unmuted = "Du hast <b>%player%</b> erfolgreich entmuted!<br>Bitte warte einige Sekunden, damit der Unmute Vorgang abgeschlossen werden kann.";
 
+# Index
 $messages_index_manage_reports = "Reports verwalten";
 $messages_index_manage_reports_info = "Derzeit sind %reports% offene Reports vorhanden.";
 $messages_index_manage_reports_button = "Report öffnen";
@@ -57,9 +62,11 @@ $messages_index_check_player_info = "Suche nach Spieler UUID oder Namen.";
 $messages_index_global_bans = "Globale Bans:";
 $messages_index_global_mutes = "Globale Mutes:";
 
+# Ban/Mute list
 $messages_banlist = "Gebannte Spieler";
 $messages_mutelist = "Gemutete Spieler";
 
+# Check
 $messages_check = "Spieler überprüfen";
 $messages_check_unban = "Spieler entbannen";
 $messages_check_unmute = "Spieler entmuten";
@@ -75,6 +82,7 @@ $messages_check_ban_mute = "Ban & Mute Verlauf";
 $messages_check_ban_mute_bans = "Bans: <b>%bans%</b>";
 $messages_check_ban_mute_mutes = "Mutes: <b>%mutes%</b>";
 
+# Report
 $messages_report = "Report";
 $messages_report_jump = "Zum Server springen";
 $messages_report_close = "Report schließen";
