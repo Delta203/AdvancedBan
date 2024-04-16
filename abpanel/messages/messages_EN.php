8a7029455d241bc2a062736d3166ac8df168941e
Delta203 <ddelta203@gmail.com>
2024-04-16 12:07:27 +0200

    Update messages_EN.php

--- abpanel/messages/messages_EN.php
+++ abpanel/messages/messages_EN.php
@@ -14,6 +14,7 @@
 
 $messages_title = "Advanced Ban Panel";
 
+# Universal
 $messages_name = "Name";
 $messages_uuid = "UUID";
 $messages_key = "Key";
@@ -29,6 +30,7 @@ $messages_duration = "Duration";
 $messages_server = "Server";
 $messages_message = "Message";
 
+# Assets
 $messages_footer = "Advanced Ban - 2024 by Delta203";
 
 $messages_navbar_home = "Home";
@@ -38,17 +40,20 @@ $messages_sidebar_uuid = "UUID: <b>%uuid%</b>";
 $messages_sidebar_session = "Session: <b>%key%</b>";
 $messages_sidebar_server = "Server: <b>%server%</b>";
 
+# Error
 $messages_invalid_session = "Invalid Session";
 $messages_invalid_session_info = "Either the uuid does not match the key, the key is incorrect or the session has expired.";
 
 $messages_not_found = "Not Found";
 $messages_not_found_info = "The requested URL was not found on this server.";
 
+# Alerts
 $messages_alert_success = "You have successfully completed the report!";
 $messages_alert_jumped = "You will now jump to the server <b>%server%</b>!";
 $messages_alert_unbanned = "You have successfully unbanned <b>%player%</b>!<br>Wait a few seconds for the unban to process.";
 $messages_alert_unmuted = "You have successfully unmuted <b>%player%</b>!<br>Wait a few seconds for the unmute to process.";
 
+# Index
 $messages_index_manage_reports = "Manage Reports";
 $messages_index_manage_reports_info = "There are currently %reports% open reports.";
 $messages_index_manage_reports_button = "Open Report";
@@ -57,9 +62,11 @@ $messages_index_check_player_info = "Search for player UUID or name.";
 $messages_index_global_bans = "Global Bans:";
 $messages_index_global_mutes = "Global Mutes:";
 
+# Ban/Mute list
 $messages_banlist = "Banned Players";
 $messages_mutelist = "Muted Players";
 
+# Check
 $messages_check = "Check Player";
 $messages_check_unban = "Unban Player";
 $messages_check_unmute = "Unmute Player";
@@ -75,6 +82,7 @@ $messages_check_ban_mute = "Ban & Mute History";
 $messages_check_ban_mute_bans = "Bans: <b>%bans%</b>";
 $messages_check_ban_mute_mutes = "Mutes: <b>%mutes%</b>";
 
+# Report
 $messages_report = "Report";
 $messages_report_jump = "Jump To Server";
 $messages_report_close = "Close Report";
